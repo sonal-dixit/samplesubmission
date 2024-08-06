5b1436dca05b74228180d402254478d7f0bc8f6e
Sonal Dixit <sonal@kapso.in>
2024-08-06 11:01:50 +0530

    rectified errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Tab, Tabs } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 import './index.css'; // Assuming you have a CSS file for custom styles
-import SearchHeader from "./components/SearchHeader";
-import AddItemHeader from "./components/AddItemHeader"
 const App = () => {
   return (
     <>
@@ -12,28 +10,23 @@ const App = () => {
         <Tabs className="gap-2 custom-tabs"> {/* Added custom class for styling */}
           <Tab key="1" title="  " className="custom-tab">
             <div className="flex flex-col gap-2">
-                <SearchHeader/>
           
             </div>
           </Tab>
           <Tab key="2" title="  " className="custom-tab">
-            <div className="flex flex-col gap-2">
-            <SearchHeader/>
+            <div className="flex flex-col gap-2">\
             </div>
           </Tab>
           <Tab key="3" title="  " className="custom-tab">
             <div className="flex flex-col gap-2">
-            <AddItemHeader/>
             </div>
           </Tab>
           <Tab key="4" title="  " className="custom-tab">
             <div className="flex flex-col gap-2">
-            <AddItemHeader/>
             </div>
           </Tab>
           <Tab key="5" title="  " className="custom-tab">
             <div className="flex flex-col gap-2">
-            <AddItemHeader/>
             </div>
           </Tab>
         </Tabs>
